dfb5c865aa8a6530b7bf1ecac576968b11ae1298
GulpeIT <grishawork-01@mail.ru>
2023-09-22 00:03:33 +0300

    Auto layout button

--- app/src/main/java/com/gulpeit/laboratoryworktwo/Activity1.java
+++ app/src/main/java/com/gulpeit/laboratoryworktwo/Activity1.java
@@ -3,12 +3,27 @@ package com.gulpeit.laboratoryworktwo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Activity1 extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
-    }
+
+        button1 = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        }
+
+
 }
